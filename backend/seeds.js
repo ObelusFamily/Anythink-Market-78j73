5a2c7d2ff19097244457f03e97e4e6f651712bbb
Jeremy Shearer <jeremyshearer@icloud.com>
2022-07-02 07:11:18 -0500

    add seed script

---
+++ backend/seeds.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+require("./models/User");
+require("./models/Item");
+require("./models/Comment");
+
+mongoose.connect(
+  `${process.env.MONGODB_URI}/admin`, 
+  { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log("MONGOOSE DB CONNECTED")
+  }).catch((e) => {
+    console.error(e)
+  })
+
+
+var Item = mongoose.model("Item");
+var Comment = mongoose.model("Comment");
+var User = mongoose.model("User");
+
+
+const seed = async () => {
+  await User.deleteMany({})
+  await Item.deleteMany({})
+  await Comment.deleteMany({})
+  for (let i = 0;i<100;i++) {
+    let user = new User({
+      username: `test${i}`,
+      email: `test-${i}@example.com`,
+    })
+    await user.save()
+    let item = new Item({
+      title: `product ${i}`,
+      description: 'product description',
+      seller: user
+    })
+    await item.save()
+    let comment = new Comment({
+      seller: user,
+      item,
+      body: `Comment ${i}`
+    })
+    await comment.save()
+    console.log(`saving item ${i}`)
+  }
+}
+
+seed()
+process.exit()
