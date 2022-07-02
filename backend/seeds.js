d2e16ead81725ddcd106640ffebc0344f499f834
Jeremy Shearer <jeremyshearer@icloud.com>
2022-07-02 08:49:54 -0500

    wait for process

--- backend/seeds.js
+++ backend/seeds.js
@@ -3,45 +3,43 @@ require("./models/User");
 require("./models/Item");
 require("./models/Comment");
 
-mongoose.connect(
-  process.env.MONGODB_URI, 
-  { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log("MONGOOSE DB CONNECTED")
-  }).catch((e) => {
-    console.error(e)
-  })
-
-
-var Item = mongoose.model("Item");
-var Comment = mongoose.model("Comment");
-var User = mongoose.model("User");
-
-
 const seed = async () => {
-  await User.deleteMany({})
-  await Item.deleteMany({})
-  await Comment.deleteMany({})
-  for (let i = 0;i<100;i++) {
-    let user = new User({
-      username: `test${i}`,
-      email: `test-${i}@example.com`,
-    })
-    await user.save()
-    let item = new Item({
-      title: `product ${i}`,
-      description: 'product description',
-      seller: user
-    })
-    await item.save()
-    let comment = new Comment({
-      seller: user,
-      item,
-      body: `Comment ${i}`
-    })
-    await comment.save()
-    console.log(`saving item ${i}`)
+  try {
+      mongoose.connect(
+      process.env.MONGODB_URI, 
+      { useNewUrlParser: true, useUnifiedTopology: true })
+      var Item = mongoose.model("Item");
+      var Comment = mongoose.model("Comment");
+      var User = mongoose.model("User");
+      await User.deleteMany({})
+      await Item.deleteMany({})
+      await Comment.deleteMany({})      
+      for (let i = 0;i<100;i++) {
+        let user = await User.create({
+          username: `test${i}`,
+          email: `test-${i}@example.com`,
+        })
+        
+        let item = await Item.create({
+          title: `product ${i}`,
+          description: 'product description',
+          seller: user
+        })
+        
+        await Comment.create({
+          seller: user,
+          item,
+          body: `Comment ${i}`
+        })
+        console.log(`saved item ${i}`)        
+      }
+  } catch(err) {
+    console.error({err: JSON.stringify(err)})
   }
 }
 
-seed()
-process.exit()
+seed().then(() => {
+  mongoose.disconnect().then(() => {
+    process.exit()
+  })
+})
